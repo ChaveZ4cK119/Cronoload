67c66db7a2f0144e0d93c7234d145fc9f7df66c8
Eddy Chavez <echavezadarmes4@gmail.com>
2024-02-21 19:05:43 -0500

    se han agregado todos los archivos al repositorio

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Cronoload</title>
+</head>
+<body>
+<header>
+    <div class="titu">
+        <img src="https://certificadossena.net/wp-content/uploads/2022/10/logo-sena-naranja-png-2022.png" alt="" width="70px" height="70px">
+        <div class="titu2">
+            <h3>ADSO</h3>
+        <h4>2620480</h4>
+        </div>
+    </div>
+    
+    <div class="user">
+        <img src="https://cdn-icons-png.freepik.com/512/456/456212.png" alt="" width="50px">
+    <h4>Juanito perez</h4>
+    </div>
+    
+    
+
+</header>
+
+<div class="b1">
+    <div class="bb1">
+        <img src="https://marketplace.canva.com/EAFLKQa4XFo/2/0/1600w/canva-calendario-2024-enero-a-diciembre-con-semanas-minimalista-imprimible-blanco-v1N054xYUIk.jpg" class="crono"alt="">
+    </div>
+    <div class="bb2">
+        <div class="bbb1">
+            <div class="n1">
+                <div class="n2">
+                    <img src="img/klipartz.com.png" alt="" width="20px" height="20px" class="nn">
+                    <h4>Notificaciones</h4>
+                </div>
+                <ul class="lista">
+                    <li><a href="">
+                        opcion
+                    </a></li>
+                    <li><a href="">
+                        opcion
+                    </a></li>
+                    <li> <a href="">
+                        opcion
+                    </a></li>
+                </ul>
+                <!-- <img src="img/klipartz.com (1).png" alt="" width="20px" height="20px"> -->
+               
+            </div>
+            
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
